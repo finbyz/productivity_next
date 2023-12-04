00b0f30411917c736d4e97b0265d812d71d86904
FinByz <info@finbyz.tech>
2023-12-04 11:01:08 +0530

    changes

---
+++ productivity_next/productivity_next/doctype/screenshot_log/screenshot_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Finbyz Tech Pvt Ltd and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Screenshot Log', {
+	// refresh: function(frm) {
+
+	// }
+});
