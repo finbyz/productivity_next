deca2bf9189b856866c6af869c3ebc5deca1a97a
FinByz <info@finbyz.tech>
2024-05-29 10:36:32 +0530

    APPLICATION USAGE LOG PROCESS ID MD

--- productivity_next/productivity_next/doctype/application_usage_log/application_usage_log.py
+++ productivity_next/productivity_next/doctype/application_usage_log/application_usage_log.py
@@ -12,12 +12,12 @@ class ApplicationUsagelog(Document):
 		if not self.ip_address:
 			x_forwarded_for = frappe.get_request_header("X-Forwarded-For", str(frappe.request.headers))
 			self.ip_address = re.search(r"^(.+)", x_forwarded_for).group(1)
-		application_names = {"code.exe":"Visual Studio Code","chrome.exe":"Google c"}
+		application_names = {"code.exe":"Visual Studio Code","chrome.exe":"Google Chrome","explorer.exe":"File Explorer"}
 		if self.process_id:
 			application_name = application_names.get(self.process_id, self.process_id)
 			if application_name:
 				self.application_name = application_name
 			else:
-				self.application_name = self.process_id
+				self.application_name = self.process_id.split(".exe")[0]
 		
 		self.duration = time_diff_in_seconds(self.to_time, self.from_time)
