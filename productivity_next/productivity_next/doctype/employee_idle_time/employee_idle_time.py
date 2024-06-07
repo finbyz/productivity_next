4e5feb15bbdac0b0534b2708986127083027155a
FinByz <info@finbyz.tech>
2024-06-07 09:36:08 +0530

    EMPLOYEE IDLE TIME DOCTYPE MD

--- productivity_next/productivity_next/doctype/employee_idle_time/employee_idle_time.py
+++ productivity_next/productivity_next/doctype/employee_idle_time/employee_idle_time.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class IdleTime(Document):
+class EmployeeIdleTime(Document):
 	pass
--- productivity_next/productivity_next/doctype/employee_idle_time/test_employee_idle_time.py
+++ productivity_next/productivity_next/doctype/employee_idle_time/test_employee_idle_time.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestIdleTime(FrappeTestCase):
+class TestEmployeeIdleTime(FrappeTestCase):
 	pass
