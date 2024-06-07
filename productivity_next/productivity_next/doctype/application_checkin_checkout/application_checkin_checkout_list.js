39752409d4813eb95def554744d1fb2f76128362
FinByz <info@finbyz.tech>
2024-06-07 10:46:08 +0530

    APPLICATION CHECKING CHECKOUT MD

--- productivity_next/productivity_next/doctype/application_checkin_checkout/application_checkin_checkout_list.js
+++ productivity_next/productivity_next/doctype/application_checkin_checkout/application_checkin_checkout_list.js
@@ -1,25 +1,10 @@
 frappe.listview_settings['Application Checkin Checkout'] = {
     add_fields: ["status"],
-    formatters:{
-        status(v, df, doc){
-            if(v==='In'){
-                return `
-                <div class="list-row-col hidden-xs ellipsis">
-					<span class="indicator-pill orange filterable no-indicator-dot ellipsis" data-filter="status,=,Out" title="Document is in draft state">
-        				<span class="ellipsis">Out</span>
-        			</span>
-        		</div>
-                `;
-            }
-            return `
-            <div class="list-row-col hidden-xs ellipsis">
-					<span class="indicator-pill green filterable no-indicator-dot ellipsis" data-filter="status,=,In" title="Document is in draft state">
-        				<span class="ellipsis"> In</span>
-        			</span>
-        	</div>
-            `;
-            
+    get_indicator(doc) {
+        if (doc.status === 'Out') {
+            return [__("OUT"), "red", "status,=,Out"];
+        } else {
+            return [__("IN"), "green", "status,=,In"];
         }
-    }
-};
-
+    },
+}
