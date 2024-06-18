fe4a3e4a4ba7f3a6cbda0ac71fedee39df729f89
FinByz <info@finbyz.tech>
2024-06-18 23:06:25 +0530

    pushing url access log --anubhav

--- productivity_next/productivity_next/doctype/url_access_log/url_access_log.py
+++ productivity_next/productivity_next/doctype/url_access_log/url_access_log.py
@@ -4,9 +4,12 @@
 # import frappe
 from frappe.model.document import Document
 from frappe.utils import time_diff_in_seconds
+import frappe
 
 
 class URLAccessLog(Document):
 	def validate(self):
+		self.domain=self.url.split('/')[2]
 		self.duration = time_diff_in_seconds(self.to_time, self.from_time)
-
+		if self.duration <=0:
+		    raise frappe.ValidationError("To time should be greater than from time")
