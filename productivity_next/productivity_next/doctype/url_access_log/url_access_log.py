573b06f05d6c856616a7a9f37359f449cb09fb19
finbyz <info@finbyz.tech>
2024-07-04 09:18:00 +0530

    code for remove duplicate entry in url log --anubhav

--- productivity_next/productivity_next/doctype/url_access_log/url_access_log.py
+++ productivity_next/productivity_next/doctype/url_access_log/url_access_log.py
@@ -3,16 +3,24 @@
 
 # import frappe
 from frappe.model.document import Document
-from frappe.utils import time_diff_in_seconds
+from frappe.utils import time_diff_in_seconds,get_datetime
 import frappe
+import datetime
 
 
 class URLAccessLog(Document):
-	def validate(self):
-		self.url = self.url or ''
-		url_split = self.url.split('/')
-		if len(url_split) > 3:
-			self.domain=self.url.split('/')[2]
-		self.duration = time_diff_in_seconds(self.to_time, self.from_time)
-		if self.duration <=0:
-		    raise frappe.ValidationError("To time should be greater than from time")
+    def validate(self):
+        from_time = get_datetime(self.from_time)
+        to_time = get_datetime(self.to_time)
+        if frappe.db.exists("URL Access Log",{"employee":self.employee,"from_time":from_time,"to_time":to_time,"url":self.url}) or frappe.db.exists("URL Access Log",{"employee":self.employee,"from_time":from_time,"to_time":to_time-datetime.timedelta(seconds=1),"url":self.url}):
+            frappe.throw("URL Access Log already exists for this time period.")
+        if frappe.db.exists("URL Access Log",{"employee":self.employee,"from_time":from_time,"to_time":to_time+datetime.timedelta(seconds=1),"url":self.url}):
+            name=frappe.get_doc("URL Access Log",{"employee":self.employee,"from_time":from_time,"to_time":to_time+datetime.timedelta(seconds=1),"url":self.url},pluck="name")
+            frappe.delete_doc("URL Access Log",name)
+        self.url = self.url or ''
+        url_split = self.url.split('/')
+        if len(url_split) > 3:
+            self.domain=self.url.split('/')[2]
+        self.duration = time_diff_in_seconds(to_time, from_time)
+        if self.duration <=0:
+            raise frappe.ValidationError("To time should be greater than from time")
