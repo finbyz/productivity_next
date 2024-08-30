81aa76f5e21168151b8a017ccde6dd8d20f76a2b
finbyz <info@finbyz.tech>
2024-08-30 18:04:06 +0530

    FEW CHANGES MD

--- productivity_next/productivity_next/doctype/productify_subscription/productify_subscription.js
+++ productivity_next/productivity_next/doctype/productify_subscription/productify_subscription.js
@@ -23,7 +23,8 @@ frappe.ui.form.on("Productify Subscription", {
                 frappe.db.get_list('Employee', {
                     fields: ['name'],
                     filters: {
-                        'status': 'Active'
+                        'status': 'Active',
+                        "user_id": ["is", "set"]
                     },
                     order_by: 'name'
                 }).then((employees) => {
@@ -71,7 +72,8 @@ frappe.ui.form.on("Productify Subscription", {
         frappe.db.get_list('Employee', {
             fields: ['name', 'user_id', 'first_name',"middle_name","last_name"],
             filters: {
-                'status': 'Active'
+                'status': 'Active',
+                "user_id": ["is", "set"]
             },
             order_by: 'name'
         }).then((employees) => {
@@ -80,16 +82,12 @@ frappe.ui.form.on("Productify Subscription", {
                 if (frm.doc.list_of_users.find((user) => user.employee === employee.name)) {
                     return;
                 }
-                full_name = frappe.utils.comma_sep([employee.first_name, employee.middle_name, employee.last_name],sep=' ');
-                full_name = full_name.replace(/,/g, '');
-                console.log(full_name)
                 table_data.push({
                     'employee': employee.name,
                     'fincall': 1,
                     'application_usage': 1,
                     'sales_person': 1,
                     'user_id': employee.user_id,
-                    "employee_name": full_name,
                 }); 
             });
             frm.set_value('list_of_users', [...frm.doc.list_of_users, ...table_data]);
