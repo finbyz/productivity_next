fb54641c77ce74e65da65a4e031b5352783120f0
Finbyz Tech <info.finbyz.com>
2024-12-12 11:49:17 +0530

    PS changes for field in auto repeat

--- productivity_next/hooks.py
+++ productivity_next/hooks.py
@@ -90,6 +90,7 @@ scheduler_events = {
 }
 
 fixtures = [
+    {"dt": "Custom Field", "filters": [["dt", "in", ["Auto Repeat"]]]},
     {"dt": "Property Setter", "filters": [["module", "in", ["Productivity Next"]]]},
 ]
 
