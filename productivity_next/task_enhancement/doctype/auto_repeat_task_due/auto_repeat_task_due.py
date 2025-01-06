8c7bca5ca0472e558902bc28232ca4551a4ab0d8
Finbyz Tech <info.finbyz.com>
2025-01-06 13:01:51 +0530

    Auto Repeat Update

--- productivity_next/task_enhancement/doctype/auto_repeat_task_due/auto_repeat_task_due.py
+++ productivity_next/task_enhancement/doctype/auto_repeat_task_due/auto_repeat_task_due.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024, Finbyz Tech Pvt Ltd and contributors
+# Copyright (c) 2025, Finbyz Tech Pvt Ltd and contributors
 # For license information, please see license.txt
 
 # import frappe
