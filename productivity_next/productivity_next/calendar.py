6b11c08c5140908b1bedd1b463d0d7e0992bb61f
Finbyz Tech <info.finbyz.com>
2025-01-10 15:32:40 +0530

    CALENDAR CHANGES MD

--- productivity_next/productivity_next/calendar.py
+++ productivity_next/productivity_next/calendar.py
@@ -3,37 +3,24 @@ import json
 
 @frappe.whitelist()
 def get_custom_events(doctype, start=None, end=None, filters=None):
-    conditions = []
-    if start and end:
-        date_conditions = f"""
-            (exp_start_date IS NULL OR exp_end_date IS NULL OR 
-             (exp_start_date <= '{end}' AND exp_end_date >= '{start}'))
-        """
-        conditions.append(date_conditions)
-    else:
-        conditions.append("(exp_start_date IS NULL OR exp_end_date IS NULL)")
-    if filters:
-        if isinstance(filters, str):
-            filters = json.loads(filters)
-        if isinstance(filters, list):
-            if len(filters) > 0 and len(filters[0]) == 5:
-                field = filters[0][1]
-                value = filters[0][3]
-                conditions.append(f"`{field}` BETWEEN '{value[0]}' AND '{value[1]}'")
+    filters = json.loads(filters) if filters else []
+    condition = ""
 
-            elif len(filters) > 0 and len(filters[0]) == 3:
-                field = filters[0][0]
-                value = filters[0][2]
-                conditions.append(f"`{field}` = '{value}'")            
-        elif isinstance(filters, dict):
-            for field, value in filters.items():
-                conditions.append(f"`{field}` = '{value}'")
-    condition_str = " AND ".join(conditions)
-        
+    for filter in filters:
+        field = filter[1]
+        operator = filter[2]
+        value = filter[3]
+
+        if operator.upper() == "BETWEEN" and isinstance(value, list) and len(value) == 2:
+            # Properly format the BETWEEN clause
+            condition += f" AND {field} BETWEEN '{value[0]}' AND '{value[1]}'"
+        else:
+            # Handle other operators
+            condition += f" AND {field} {operator} '{value}'"
     query = f"""
         SELECT *
         FROM `tab{doctype}`
-        WHERE {condition_str}
+        WHERE 1=1{condition}
     """
     events = frappe.db.sql(query, as_dict=True)
     for event in events:
