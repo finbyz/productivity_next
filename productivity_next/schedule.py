b8d3050b266f47a481683d6e480a47cfe2aac577
finbyz <info@finbyz.com>
2025-03-20 18:19:02 +0530

    timesheet logs creation using PWS

--- productivity_next/schedule.py
+++ productivity_next/schedule.py
@@ -1,5 +1,5 @@
 import frappe
-from frappe.utils import nowdate, get_datetime, format_time, format_duration
+from frappe.utils import nowdate, get_datetime, format_time, format_duration,today
 import frappe.utils
 from frappe.utils.data import cint
 from productivity_next.productivity_next.page.productify_consolidated_analysis.productify_consolidated_analysis import user_analysis_data
@@ -1144,3 +1144,80 @@ def update_due_period():
         frappe.db.set_value("Task", record.get("name"), "due_period", due_period)
 
     frappe.db.commit()
+
+
+def merge_continuous_logs(logs):
+    if not logs:
+        return []
+    
+    logs.sort(key=lambda x: x["from_time"])  
+    merged_logs = [logs[0]]
+
+    for log in logs[1:]:
+        last_log = merged_logs[-1]
+        time_gap = (log["from_time"] - last_log["to_time"]).total_seconds()
+
+        if time_gap <= 10 and log["task"] == last_log["task"]:
+            last_log["to_time"] = log["to_time"]
+        elif time_gap <= 10 and log["issue"] == last_log["issue"]:
+            last_log["to_time"] = log["to_time"]
+        elif time_gap <= 10 and log["project"] == last_log["project"]:
+            last_log["to_time"] = log["to_time"]
+        elif time_gap <= 10 and log["meeting"] == last_log["meeting"]:
+            last_log["to_time"] = log["to_time"]
+        else:
+            log['from_time'] = log['from_time'] + timedelta(seconds=1)
+            merged_logs.append(log)
+
+    return merged_logs
+
+
+
+def create_timesheet_logs():
+    work_sumaries = frappe.get_all(
+        "Productify Work Summary",
+        filters={"date": today()},
+        fields=["name", "date", "employee"]
+    )
+    
+    for work_summary in work_sumaries:
+        applications = frappe.get_all("Productify Work Summary Application", filters={"parent": work_summary["name"]}, fields=[ "meeting", "issue", "task", "project", "from_time", "to_time"])
+        existing_timesheets = frappe.get_all(
+            "Timesheet",
+            filters={"employee": work_summary["employee"], "status": "Draft"},
+            fields=["name"]
+        )
+
+        if existing_timesheets:
+            timesheet = frappe.get_doc("Timesheet", existing_timesheets[0]["name"])
+            timesheet.time_logs = []
+        else:
+            timesheet = frappe.new_doc("Timesheet")
+            timesheet.employee = work_summary["employee"]
+        timesheet.is_created_by_productify = True
+        merged_logs = merge_continuous_logs(applications)
+        for log in merged_logs:
+            seconds = (log["to_time"] - log["from_time"]).total_seconds()
+            hours = seconds / 3600
+            if seconds < 0:
+                continue
+            activity_type = ""
+            if log.get("meeting"):
+                activity_type = "Meeting"
+            elif log.get("issue"):
+                activity_type = "Issue"
+            elif log.get("task"):
+                activity_type = "Task"
+            elif log.get("project"):
+                activity_type = "Project"
+            
+            timesheet.append("time_logs", {
+                "activity_type": activity_type,
+                "task": log.get("task"),
+                "project": log.get("project"),
+                "issue": log.get("issue"),
+                "hours": hours,
+                "from_time": log["from_time"],
+                "to_time": log["to_time"]
+            })
+        timesheet.save()
