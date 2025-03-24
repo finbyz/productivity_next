7b63c2c1e934ce798ebd0d31934edc5a652d84e6
finbyz <info@finbyz.com>
2025-03-24 17:50:44 +0530

    fix: delete yesterday timesheet patch

--- productivity_next/patches/delete_yesterday_timesheet.py
+++ productivity_next/patches/delete_yesterday_timesheet.py
@@ -3,9 +3,15 @@ from frappe.utils import nowdate, add_days
 
 def execute():
     yesterday = add_days(nowdate(), -1)
-    timesheets = frappe.get_all("Timesheet", filters={"creation": [">=", yesterday], "creation": ["<", nowdate()]}, pluck="name")
+    timesheets = frappe.get_all(
+        "Timesheet", 
+        filters={
+            "creation": ["between", [yesterday, yesterday]],
+            "docstatus": 1
+        },
+        pluck="name"
+    )
     for ts in timesheets:
-        doc = frappe.get_doc("Timehsheet", ts)
+        doc = frappe.get_doc("Timesheet", ts)
         doc.cancel()
         frappe.delete_doc("Timesheet", ts, force=True)
-    frappe.db.commit()
