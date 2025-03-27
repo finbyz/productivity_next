d0cf2b43ad0229b162ead473f4fd083e9fc6ee96
finbyz <info@finbyz.com>
2025-03-27 14:34:24 +0530

    Or filter removed

--- productivity_next/task_enhancement/report/task_analysis/task_analysis.py
+++ productivity_next/task_enhancement/report/task_analysis/task_analysis.py
@@ -213,8 +213,8 @@ def add_task_to_data(data, task, parent_children_map, level, show_progress=False
             
 
 def get_tasks(filters): 
-    task_filters = {}
-    task_or_filters = {"is_template": 0}
+    task_filters = {"is_template": 0}
+    task_or_filters = {}
 
     # Filter by project
     if filters.get('project'):
