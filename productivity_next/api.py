1f443f8e44fe155896a5a4ec87830db3f79c0101
Finbyz <info@finbyz.tech>
2025-04-04 12:22:47 +0530

    remove get_all and use get_list in working task list

--- productivity_next/api.py
+++ productivity_next/api.py
@@ -1977,7 +1977,7 @@ def get_defaults_productivity():
 
 @frappe.whitelist()
 def get_employee_working_tasks():
-    employees = frappe.get_all('Employee', fields=['name', 'employee_name', 'user_id'], filters={'status': 'Active'})
+    employees = frappe.get_list('Employee', fields=['name', 'employee_name', 'user_id'], filters={'status': 'Active'})
     latest_logs = frappe.db.sql(f"""
         WITH ranked_logs AS (
             SELECT 
