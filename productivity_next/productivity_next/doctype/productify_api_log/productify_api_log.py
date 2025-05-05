258d2221920c0358930e77b756db347206844832
Finbyz <info@finbyz.tech>
2025-05-05 12:36:29 +0530

    feat: productify call log doctype added

---
+++ productivity_next/productivity_next/doctype/productify_api_log/productify_api_log.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Finbyz Tech Pvt Ltd and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ProductifyAPILog(Document):
+	pass
