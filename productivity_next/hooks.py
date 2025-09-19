c55ca39716049f0a3e3182515cb2ffc6d44bfcb8
Finbyz <info@finbyz.tech>
2025-09-19 07:11:54 +0000

    PS uncomment timesheet scheduler in hooks file

--- productivity_next/hooks.py
+++ productivity_next/hooks.py
@@ -93,7 +93,7 @@ scheduler_events = {
             "productivity_next.schedule.create_auto_email_report_weekly",
         ],
         "0 0 * * *": [
-            # "productivity_next.schedule.submit_timesheet_created_by_productify",
+            "productivity_next.schedule.submit_timesheet_created_by_productify",
             "productivity_next.schedule.update_due_period",
         ],
         "0 1 * * 2-7": [
