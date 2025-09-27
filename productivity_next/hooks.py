41a3a1465c8e1ade73e3f7c7199ab61e60ca522e
Finbyz <info@finbyz.tech>
2025-09-27 06:00:26 +0000

    PS hooks changes for timesheet

--- productivity_next/hooks.py
+++ productivity_next/hooks.py
@@ -99,9 +99,9 @@ scheduler_events = {
         "0 1 * * 2-7": [
             "productivity_next.api.task_mail_remainder",
         ],
-        # "0 23 * * *" : [
-        #     "productivity_next.schedule.create_timesheet_logs"
-        # ],
+        "0 23 * * *" : [
+            "productivity_next.schedule.create_timesheet_logs"
+        ],
 
         # "5 4 * * sun" :[
         #     "productivity_next.schedule.send_weekly_report",
